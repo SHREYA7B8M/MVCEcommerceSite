337d03ca1eb32066dcec7bdf6388971d633c5a06
s66829020@gmail.com <s66829020@gmail.com>
2023-09-08 20:16:20 +0530

    first commit

---
+++ PhaseEnd2/PhaseEnd2/Data/PhaseEnd2DbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PhaseEnd2.Models;
+
+namespace PhaseEnd2.Data
+{
+    public class PhaseEnd2DbContext : DbContext
+    {
+        public PhaseEnd2DbContext (DbContextOptions<PhaseEnd2DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PhaseEnd2.Models.Order> Order { get; set; } = default!;
+
+        public DbSet<PhaseEnd2.Models.OrderDetail>? OrderDetail { get; set; }
+
+        public DbSet<PhaseEnd2.Models.Product>? Product { get; set; }
+    }
+}
